75e6c2ccbfd7d67c58f366df7c697270dfea7b38
Matej Franjić <mfranjic21@foi.hr>
2023-12-03 22:23:50 +0100

    Početne postavke aplikacije. Izrađeni slojevi arhitekture, forme aplikacije i repozitorij.

---
+++ Software/Iznajmljivanje Vozila/Forms/frmTransactionHistory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Iznajmljivanje_Vozila.Forms
+{
+    public partial class frmTransactionHistory : Form
+    {
+        public frmTransactionHistory()
+        {
+            InitializeComponent();
+        }
+    }
+}
