e47d51dd89fc96164e96133e2b26c8fe4619228f
Marko Saško <msasko1@foi.hr>
2024-01-28 10:56:04 +0100

    Kreirane forme za upravljanje rezervacijama i dodavanje rezervacija #5

--- Software/Iznajmljivanje Vozila/Forms/frmReservation.Designer.cs
+++ Software/Iznajmljivanje Vozila/Forms/frmReservation.Designer.cs
@@ -1,4 +1,7 @@
-﻿namespace Iznajmljivanje_Vozila.Forms
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace Iznajmljivanje_Vozila.Forms
 {
     partial class frmReservation
     {
@@ -28,20 +31,83 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.dgvReservations = new System.Windows.Forms.DataGridView();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvReservations)).BeginInit();
             this.SuspendLayout();
+            // 
+            // dgvReservations
+            // 
+            this.dgvReservations.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvReservations.Location = new System.Drawing.Point(15, 72);
+            this.dgvReservations.Name = "dgvReservations";
+            this.dgvReservations.RowTemplate.Height = 25;
+            this.dgvReservations.Size = new System.Drawing.Size(665, 238);
+            this.dgvReservations.TabIndex = 0;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(139, 316);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(64, 20);
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.Text = "Dodaj";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(274, 316);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(64, 20);
+            this.btnUpdate.TabIndex = 2;
+            this.btnUpdate.Text = "Ažuriraj";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(406, 316);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(64, 20);
+            this.btnDelete.TabIndex = 3;
+            this.btnDelete.Text = "Obriši";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(536, 316);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(64, 20);
+            this.btnClose.TabIndex = 4;
+            this.btnClose.Text = "Zatvori";
+            this.btnClose.UseVisualStyleBackColor = true;
             // 
             // frmReservation
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(698, 347);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.dgvReservations);
             this.Name = "frmReservation";
+            this.Padding = new System.Windows.Forms.Padding(3, 55, 3, 3);
             this.Text = "Rezervacije";
-            this.Load += new System.EventHandler(this.frmReservation_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvReservations)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private DataGridView dgvReservations;
+        private Button btnAdd;
+        private Button btnUpdate;
+        private Button btnDelete;
+        private Button btnClose;
     }
 }
