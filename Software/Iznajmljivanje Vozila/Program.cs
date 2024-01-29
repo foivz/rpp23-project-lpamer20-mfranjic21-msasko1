d657e16b23b35fc0789583758d80939abcbc3a7e
Marko Saško <msasko1@foi.hr>
2024-01-29 19:15:23 +0100

    Promjena da se aplikacija otvara na formi za prijavu.

--- Software/Iznajmljivanje Vozila/Program.cs
+++ Software/Iznajmljivanje Vozila/Program.cs
@@ -17,7 +17,7 @@ namespace Iznajmljivanje_Vozila
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FrmMain());
+            Application.Run(new FrmLogin());
         }
     }
 }
